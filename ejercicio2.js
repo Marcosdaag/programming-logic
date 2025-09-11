7de7dc45caa9fc12f0065b6c44fe627623d2458b
Marcosdaag <marcosoffs99@gmail.com>
2025-09-11 07:59:16 -0300

    ejercicios 3 y 4

--- ejercicio2.js
+++ ejercicio2.js
@@ -18,3 +18,10 @@ function palindromo(text){
 }
 
 console.log("Es un palindromo? "+palindromo("Marcos"));
+
+/* 
+Explicacion:
+
+Creamos una funcion palindromo que va a recibir en este caso un parametro de tipo String. Dentro de esta funcion creamos una variable reverse que almacena el texto ingresado como parametro y en primer lugar hace un split del texto teniendo en cuenta el parametro que le pasamos, (en este caso separamos el texto en cada caracter ya que usamos las comillas sin ningun valor '') despues hace un reverse lo que da vuelta el orden del array de caracteres que devuelve el split y por ultimo con el metodo join volvemos a concatenar ese array.
+Por ultimo comparamos si el texto dado vuelta es igual al texto original usando === que nos devuelve un boolean al igual que con el if y else de abajo, la forma en la que quedo es lo optimo y mas compacto.
+*/
---
+++ ejercicio4.js
@@ -0,0 +1,22 @@
+/*
+Enunciado ejercicio 4:
+Dada una cadena de texto, darle la vuelta e invertir el orden de sus caracteres, sin usar metodos propios del lenguaje, solo estructuras de control.
+*/
+
+function reverse(text){
+    let reversed = "";
+
+    for(let char of text){
+        reversed = char + reversed
+    }
+
+    return reversed;
+}
+
+console.log(reversed);
+
+/* 
+Explicacion:
+
+Creamos una funcion reverse que recibe como parametro un texto. Creamos una variable reversed inicializada sin ningun caracter y despues en un bucle for iteramos cada caracter del texto que le pasamos como parametro (la diferencia entre usar OF o IN en el condicional del bucle es que OF devuelve el valor de la posicion dentro del array que esta recorriendo a diferencia de IN que devuelve el index del elemento). En cada una de esas iteraciones le agregamos el character actual al reversed anterior por lo que va a estar agregando el nuevo caracter al que ya estaba previamente, por lo que de esta manera queda el texto invertido.
+*/
