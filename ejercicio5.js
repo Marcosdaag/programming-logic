be7e0a115e5b7ace24de6cedb415b983053f0f0c
Marcosdaag <marcosoffs99@gmail.com>
2025-09-12 10:20:33 -0300

    ejercicios 5 y 6

---
+++ ejercicio5.js
@@ -0,0 +1,18 @@
+/*
+Enunciado ejercicio 5:
+Crear una funcion que responda lo siguiente, cuanto es el X porcentaje de X numero.
+*/
+
+function percent(percent, number){
+    var result = (number*percent)/100;
+    // Esta es otra forma de calcularlo var result = (number*(percent/100));
+    console.log(`El %${percent} de ${number} es ${result}`);
+}
+
+percent(10, 2000);
+
+/* 
+Explicacion:
+
+Cree una funcion que recibe como parametros tanto el porcentaje que quiere y el numero del que quiere saber ese porcentaje, despues cree la variable result donde aplicando una regla de 3 simple saco lo que el usuario solicida y lo concateno en un texto dentro de un console.log
+*/
---
+++ ejercicio6.js
@@ -0,0 +1,23 @@
+/*
+Enunciado ejercicio 5:
+Dibujar un cuadrado hueco con asteriscos.
+*/
+
+function square(num){
+    for(let i = 0; i < 2; i++){
+        console.log('*'.repeat(num));
+        if ( i === 0 ) {
+            for(let x = 0; x < num-2; x++){
+                console.log('*' + ' '.repeat(num-2) + '*');
+            }
+        }
+    }
+}
+ 
+square(10);
+
+/* 
+Explicacion:
+
+En esta funcion asignamos un parametro que define la cantidad de asteriscos que se van a pintar por cada lado y lo dibujamos dentro de la consola.
+*/
