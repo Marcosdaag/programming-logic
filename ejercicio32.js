0ad68bc04ab0134b40777564b3b48597d0788c09
Marcosdaag <marcosoffs99@gmail.com>
2025-09-18 17:57:25 -0300

    ejercicio 32

--- ejercicio32.js
+++ ejercicio32.js
@@ -1,12 +1,28 @@
 /*
 Enunciado ejercicio 32:
 
-
+Dado un array de numeros, devolver el array con sus numeros elevados al cuadrado.
 */
 
+function square(array1){
 
+    // Tambien podria usar filter para limpiar los strings no deseados.
+
+    result = [];
+    
+    for(number of array1){
+        let calculate = Math.pow(number, 2);
+        result.push(calculate);
+    }
+
+    return result;
+}
+
+console.log(square([5, 6, 7]));
 
 /* 
 Explicacion:
 
+Creo una funcion square que recibe un array de numeros como parametro. Inicializo una variable result como un array vacio y hago una iteracion por cada dato dentro del array que ingresa el usuario. Por cada iteracion le doy el valor de la multiplicacion del numero por si mismo a la variable de calcualte. 
+Por ultimo hago un push del valor a la raviable result y la retorno al final.
 */
--- ejercicio33.js
+++ ejercicio33.js
@@ -1,10 +1,53 @@
 /*
 Enunciado ejercicio 33:
 
-
+Haz un reloj que en tiempo real vaya mostrando la hora a cada segundo.
+Solo puedes uasr el objeto date para obtener la hora por primera vez, pero no para actualizarla.
 */
 
+class clock{
 
+    constructor(){
+        this.date = new Date();
+        this.hours = this.date.getHours();
+        this.minutes = this.date.getMinutes();
+        this.seconds = this.date.getSeconds();
+    }
+
+    update(seconds){
+        this.seconds += seconds;
+
+        // Actualizar los segundos
+        if(seconds >= 60){
+            this.minutes++;
+            this.seconds = 0;
+        }
+
+        // Actualizar los minutos
+        if(this.minutes >= 60){
+            this.hours++;
+            this.minutes = 0;
+        }
+
+        // Actualizar las horas
+        if(this.hours >= 24){
+            this.hours = 0;
+        }
+    }
+
+    show(){
+        this.update(1);
+        console.log(`${this.hours}:${this.minutes}:${this.seconds}`);
+    }
+
+    on(){
+        
+        setInterval(()=>{
+            this.show();
+        },1000);
+    }
+
+}
 
 /* 
 Explicacion:
