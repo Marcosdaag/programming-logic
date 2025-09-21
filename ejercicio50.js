9a4c3fbc1f75b1beb1d8ec7ce5652b7754f400c7
Marcosdaag <marcosoffs99@gmail.com>
2025-09-21 19:55:07 -0300

    ejercicio 50 y 51

--- ejercicio50.js
+++ ejercicio50.js
@@ -1,10 +1,29 @@
 /*
 Enunciado ejercicio 50:
 
+Dado un numero, devolver cuantos bucles tiene.
 */
 
+function bucle(number){
+    let arrayNum = number.toString().split('');
+    let bucles = 0;
+
+    for(element of arrayNum){
+        element = parseInt(element);
+        if(element === 0 || element === 6 || element === 9){
+            bucles ++;
+        }else if(element === 8){
+            bucles += 2;
+        }
+    }
+    
+    return bucles;
+}
+
+console.log(bucle(28891)); 
 
 /* 
 Explicacion:
 
+Creo una funcion bucle que recibe como parametro un numero. Primero, convierto el numero a una cadena de texto y luego lo divido en un array de caracteres utilizando el metodo split. Inicializo una variable bucles en 0 para contar la cantidad de bucles. Luego, recorro el array de caracteres con un bucle for of. Por cada caracter, lo convierto de nuevo a un numero entero utilizando parseInt. Verifico si el numero es 0, 6 o 9, en cuyo caso incremento la variable bucles en 1. Si el numero es 8, incremento la variable bucles en 2, ya que el 8 tiene dos bucles. Finalmente, retorno la cantidad total de bucles.
 */
--- ejercicio51.js
+++ ejercicio51.js
@@ -1,10 +1,29 @@
 /*
 Enunciado ejercicio 51:
 
+Dado un array de numeros, devolver un array nuevo con los numeros pares e inpares separados.
 */
 
+function oddOrPair(array1){
+
+    let odd = [];
+    let pair = [];
+
+    for(numb of array1){
+        if(numb%2 != 0){
+            odd.push(numb);
+        }else{
+            pair.push(numb);
+        }
+    }
+
+    return `pares: ${pair} impares: ${odd}`;
+}
+
+console.log(oddOrPair([50, 45, 30, 98, 3]));
 
 /* 
 Explicacion:
 
+Creo una funcion oddOrPair que recibe como parametro un array de numeros. Inicializo dos arrays vacios, odd para almacenar los numeros impares y pair para almacenar los numeros pares. Luego, utilizo un bucle for of para recorrer cada numero del array original. Dentro del bucle, verifico si el numero es impar utilizando el operador modulo (%). Si el numero es impar (numb%2 != 0), lo agrego al array odd utilizando el metodo push. Si el numero es par, lo agrego al array pair. Finalmente, retorno una cadena de texto que muestra los numeros pares e impares separados.
 */
